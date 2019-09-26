fdced45e23d49b036ee010a9f8ef3a4b4e9dca99
MrBradabie <55830472+MrBradabie@users.noreply.github.com>
2019-09-26 16:48:39 +0700

    Add files via upload

---
+++ PBO1-10118006-Latihan8-Pecahan/src/pbo1/pkg10118006/latihan8/pecahan/PBO110118006Latihan8Pecahan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118006.latihan8.pecahan;
+
+/**
+ *
+ * @author 
+ * NAMA  : Alfarizi Rizki Pane
+ * KELAS : PBO1
+ * NIM   : 10118006
+ * Deskripsi Program : Program ini berisi  program untuk pecahan
+ */
+public class PBO110118006Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+                
+    }
+    
+}
